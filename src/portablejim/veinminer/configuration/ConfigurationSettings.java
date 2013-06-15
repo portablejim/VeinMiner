ad3023a81c3d99f04b9b19010651c6dbf4f75156
James McKay <dcseee-github@yahoo.com.au>
2013-06-16 08:55:19 +1000

    Start on stuff for configuration

---
+++ src/portablejim/veinminer/configuration/ConfigurationSettings.java
@@ -0,0 +1,182 @@
+package portablejim.veinminer.configuration;
+
+import portablejim.veinminer.util.BlockID;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: james
+ * Date: 12/06/13
+ * Time: 9:50 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ConfigurationSettings {
+
+    public ConfigurationSettings() {
+        for (ToolType tool : ToolType.values()) {
+            blockWhitelist[tool.ordinal()] = new ArrayList<BlockID>();
+        }
+    }
+
+    public enum ToolType { PICKAXE, SHOVEL, AXE  }
+
+    /**
+     * List of block IDs to whitelist for each tool.
+     */
+    private List<BlockID>[] blockWhitelist;
+
+    /**
+     * Groups congruent block IDs together in an array.
+     * Congruent blocks are blocks that should be treated as the same block.
+     */
+    private ArrayList<Set<BlockID>> blockCongruenceList;
+
+    /**
+     * A map to easily get the list of congruent block IDS for a specified block ID.
+     */
+    private HashMap<BlockID, Integer> blockCongruenceMap;
+
+    private int blockLimit;
+
+    private int radiusLimit;
+
+    private int blocksPerTick;
+
+    private boolean defaultSneakMode;
+
+    /**
+     * Add the blocks mentioned in whitelist to the block whitelist for the specified tool.
+     * @param whitelist String of blocks with metadata value to add to whitelist. Format is ':' to separate block id and
+     *                  metadata and ',' to separate blocks. e.g. "[block id]:[metadata],[block id]".
+     *                  See {@link ConfigurationValues}.
+     * @param tool
+     */
+    public void setBlockWhitelist(String whitelist, ToolType tool) {
+        String[] blocksString = whitelist.split(",");
+
+        for (String blockString : blocksString ) {
+            BlockID newBlock = new BlockID(blockString, ":");
+            blockWhitelist[tool.ordinal()].add(newBlock);
+        }
+    }
+
+    public void clearBlockWhitelist() {
+        for (ToolType tool : ToolType.values()) {
+            blockWhitelist[tool.ordinal()].clear();
+        }
+    }
+
+    public List<BlockID> getBlockWhitelist(ToolType tool) {
+        return blockWhitelist[tool.ordinal()];
+    }
+
+    public boolean whiteListHasBlockId(ToolType tool, BlockID targetBlock) {
+        return blockWhitelist[tool.ordinal()].contains(targetBlock);
+    }
+
+    /**
+     * Add groups of blocks mentioned in congruenceList to a set of blocks to be considered equal.
+     * @param congruenceList String of groups of blocks with metadata value to be added to congruence list. Format is
+     *                       ':' to separate block id and metadata, '-' to separate blocks in groups and ',' to separate
+     *                       groups. e.g. "[block id]:[metadata]-[block id];[block id]-[block id]-[block id]".
+     *                       See {@link ConfigurationValues}
+     */
+    public void setBlockCongruenceList(String congruenceList) {
+        String[] blocksString = congruenceList.split(",");
+
+        for (String congruentBlocks : blocksString) {
+            int newId = blockCongruenceList.size();
+            List<BlockID> newCongruentBlocks = new ArrayList<BlockID>();
+
+            if (!congruentBlocks.contains("-")) {
+                 continue;
+            }
+
+            for (String blockString : congruentBlocks.split("-")) {
+                BlockID newBlockId = new BlockID(blockString);
+
+                newCongruentBlocks.add(newBlockId);
+
+                if (blockCongruenceMap.containsKey(newBlockId)) {
+                    newId = blockCongruenceMap.get(newBlockId);
+                }
+
+                blockCongruenceMap.put(newBlockId, newId);
+            }
+
+            newCongruentBlocks.addAll(newCongruentBlocks);
+        }
+    }
+
+    public ArrayList<Set<BlockID>> getBlockCongruenceList() {
+        return blockCongruenceList;
+    }
+
+    public HashMap<BlockID, Integer> getBlockCongruenceMap() {
+        return  blockCongruenceMap;
+    }
+
+    public Set<BlockID> getCongruentBlocks(BlockID targetBlock) {
+        int listId = blockCongruenceMap.get(targetBlock);
+        return blockCongruenceList.get(listId);
+    }
+
+    public boolean areBlocksCongurent(BlockID block1, BlockID block2) {
+
+        if (blockCongruenceMap.containsKey(block1) && blockCongruenceMap.containsKey(block2)) {
+            return  blockCongruenceMap.get(block1) == blockCongruenceMap.get(block2);
+        }
+
+        return false;
+    }
+
+    public int getBlockLimit() {
+        return blockLimit;
+    }
+
+    public void setBlockLimit(int blockLimit) {
+        this.blockLimit = blockLimit;
+    }
+
+    public int getRadiusLimit() {
+        return radiusLimit;
+    }
+
+    public void setRadiusLimit(int radiusLimit) {
+        if (radiusLimit < -1) {
+            radiusLimit = -1;
+        }
+        else if (radiusLimit > 1000) {
+            radiusLimit = 100;
+        }
+
+        this.radiusLimit = radiusLimit;
+    }
+
+    public int getBlocksPerTick() {
+        return blocksPerTick;
+    }
+
+    public void setBlocksPerTick(int blocksPerTick) {
+        if (blocksPerTick < -1) {
+            blocksPerTick = -1;
+        }
+        else if (blocksPerTick > 1000) {
+            blocksPerTick = 100;
+        }
+
+        this.blocksPerTick = blocksPerTick;
+    }
+
+    public boolean isDefaultSneakMode() {
+        return defaultSneakMode;
+    }
+
+    public void setDefaultSneakMode(boolean defaultSneakMode) {
+        this.defaultSneakMode = defaultSneakMode;
+    }
+}
--- src/portablejim/veinminer/configuration/ConfigurationValues.java
+++ src/portablejim/veinminer/configuration/ConfigurationValues.java
@@ -8,4 +8,39 @@ package portablejim.veinminer.configuration;
  * To change this template use File | Settings | File Templates.
  */
 public class ConfigurationValues {
+
+    public String PICKAXE_BLOCK_ID_LIST;
+    public static final String PICKAKE_BLOCK_ID_LIST_DEFAULT = "14,15,16,21,24:0,21,24:0,24:1,24:2,48,56,73,89,129";
+    public static final String PICKAXE_BLOCK_ID_LIST_CONFIGNAME = "blockList.pickaxe";
+    public static final String PICKAXE_BLOCK_ID_LIST_DESCRIPTION = "Block ids to auto-mine when using a configured pickaxe. [default: '14,15,16,21,24:0,21,24:0,24:1,24:2,48,56,73,89,129']";
+
+    public String SHOVEL_BLOCK_ID_LIST;
+    public static final String SHOVEL_BLOCK_ID_LIST_DEFAULT = "82";
+    public static final String SHOVEL_BLOCK_ID_LIST_CONFIGNAME = "blockList.shovel";
+    public static final String SHOVEL_BLOCK_ID_LIST_DESCRIPTION = "Block ids to auto-mine when using a configured shovel. [default: '82']";
+
+    public String AXE_BLOCK_ID_LIST;
+    public static final String AXE_BLOCK_ID_LIST_DEFAULT = "17:0,17:1,17:2,17:3,17:3,18:0,18:1,18:2,18:3";
+    public static final String AXE_BLOCK_ID_LIST_CONFIGNAME = "blockList.axe";
+    public static final String AXE_BLOCK_ID_LIST_DESCRIPTION = "Block ids to auto-mine when using a configured axe. [default '17:0,17:1,17:2,17:3,17:3,18:0,18:1,18:2,18:3']";
+
+    public String BLOCK_EQUIVALENCY_LIST;
+    public static final String BLOCK_EQUIVALENCY_LIST_DEFAULT = "73-74";
+    public static final String BLOCK_EQUIVALENCY_LIST_CONFIGNAME = "equalBlocks";
+    public static final String BLOCK_EQUIVALENCY_LIST_DESCRIPTION = "Block IDs to consider equivalent. Separate blocks with '-'. Separate groups with ',' [default: '73-74']";
+
+    public int BLOCK_LIMIT;
+    public static final int BLOCK_LIMIT_DEFAULT = 800;
+    public static final String BLOCK_LIMIT_CONFIGNAME = "limit.blocks";
+    public static final String BLOCK_LIMIT_DESCRIPTION = "Limit of blocks to be destroyed at once. Use -1 for infinite. [range: -1 to 2147483647, default: '800']";
+
+    public int RADIUS_LIMIT;
+    public static final int RADIUS_LIMIT_DEFAULT = 6;
+    public static final String RADIUS_LIMIT_CONFIGNAME = "limit.radius";
+    public static final String RADIUS_LIMIT_DESCRIPTION = "Maximum distance from the first block to search for blocks to destroy. [range: -1 to 1000, default: '6']";
+
+    public int BLOCKS_PER_TICK;
+    public static final int BLOCKS_PER_TICK_DEFAULT = 50;
+    public static final String BLOCKS_PER_TICK_CONFIGNAME = "limit.blocksPerTick";
+    public static final String BLOCKS_PER_TICK_DESCRIPTION = "Maximum number of blocks to be removed per game tick (1/20 seconds). Using a low number will keep the game from getting huge performance drops but also decreases the speed at which blocks are destroyed. [range: 1 ~ 1000, default: 50]";
 }
