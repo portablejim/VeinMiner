10641a1dc3b00c376792cfaebdf6f5ec3f5bdfb6
James McKay <dcseee-github@yahoo.com.au>
2013-10-11 11:21:09 +1100

    Update version to 0.4. Update mod authors and credits

--- src/portablejim/veinminer/lib/ModInfo.java
+++ src/portablejim/veinminer/lib/ModInfo.java
@@ -12,7 +12,7 @@ public class ModInfo {
 
     public static final String MOD_ID = "VeinMiner";
     public static final String MOD_NAME = "Vein Miner";
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "0.4";
     public static final String PROXY_SERVER_CLASS = "portablejim.veinminer.proxy.ServerProxy";
     public static final String PROXY_CLIENT_CLASS = "portablejim.veinminer.proxy.ClientProxy";
     public static final String CHANNEL = "portablejim_vm";
