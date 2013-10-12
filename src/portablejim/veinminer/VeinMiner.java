7089f9df6d162e48b6e431ecec7ea8c5017fbb1c
James McKay <dcseee-github@yahoo.com.au>
2013-10-13 01:42:16 +1100

    Replace deprecated @Init etc. with @EventHandler

--- src/portablejim/veinminer/VeinMiner.java
+++ src/portablejim/veinminer/VeinMiner.java
@@ -20,10 +20,7 @@ package portablejim.veinminer;
 import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Instance;
-import cpw.mods.fml.common.Mod.PreInit;
-import cpw.mods.fml.common.Mod.Init;
-import cpw.mods.fml.common.Mod.PostInit;
-import cpw.mods.fml.common.Mod.ServerStarted;
+import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
@@ -68,25 +65,25 @@ public class VeinMiner {
     @SidedProxy(clientSide = ModInfo.PROXY_CLIENT_CLASS, serverSide = ModInfo.PROXY_SERVER_CLASS)
     public static CommonProxy proxy;
 
-    @PreInit
+    @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         configurationValues = new ConfigurationValues(event.getSuggestedConfigurationFile());
         proxy.setupConfig(configurationValues);
         proxy.registerKeybind();
     }
 
-    @Init
+    @EventHandler
     public void init(FMLInitializationEvent event) {
         MinecraftForge.EVENT_BUS.register(new EntityDropHook());
 
     }
 
-    @PostInit
+    @EventHandler
     public void postInit(FMLPostInitializationEvent event) {
 
     }
 
-    @ServerStarted
+    @EventHandler
     public void serverStarted(FMLServerStartedEvent event) {
         new MinerServer(configurationValues);
 
