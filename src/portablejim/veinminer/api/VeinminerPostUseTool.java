556753b82e00d3b9435d12366d03e5d3f5559d1e
James McKay <dcseee-github@yahoo.com.au>
2014-02-02 16:13:31 +1100

    Add documentation to API.

--- src/portablejim/veinminer/api/VeinminerPostUseTool.java
+++ src/portablejim/veinminer/api/VeinminerPostUseTool.java
@@ -1,15 +1,29 @@
+/* This file is part of VeinMiner.
+ *
+ *    VeinMiner is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Lesser General Public License as
+ *    published by the Free Software Foundation, either version 3 of
+ *     the License, or (at your option) any later version.
+ *
+ *    VeinMiner is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with VeinMiner.
+ *    If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package portablejim.veinminer.api;
 
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraftforge.event.Event;
 
 /**
- * Created with IntelliJ IDEA.
- * User: james
- * Date: 3/11/13
- * Time: 9:38 AM
- * To change this template use File | Settings | File Templates.
+ * Event called after a block has been destroyed.
  */
+
 public class VeinminerPostUseTool extends Event {
     public final EntityPlayerMP player;
 
