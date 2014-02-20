099bd757fa25963bfdf13e3f1ce55df98fbc5082
James McKay <dcseee-github@yahoo.com.au>
2014-02-20 14:43:30 +1100

    Add rest of main Veinminer mod file.

--- src/main/java/portablejim/veinminer/VeinMiner.java
+++ src/main/java/portablejim/veinminer/VeinMiner.java
@@ -25,13 +25,28 @@ import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.ModContainer;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLServerStartedEvent;
+import net.minecraft.block.Block;
+import net.minecraft.command.ServerCommandManager;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.MinecraftServer;
+import net.minecraftforge.oredict.OreDictionary;
+import org.apache.logging.log4j.Logger;
+import portablejim.veinminer.api.ToolType;
 import portablejim.veinminer.configuration.ConfigurationSettings;
 import portablejim.veinminer.configuration.ConfigurationValues;
 import portablejim.veinminer.lib.MinerLogger;
 import portablejim.veinminer.lib.ModInfo;
 import portablejim.veinminer.network.ChannelHandler;
 import portablejim.veinminer.proxy.CommonProxy;
+import portablejim.veinminer.server.MinerServer;
+import portablejim.veinminer.util.BlockID;
+
+import java.util.ArrayList;
+import java.util.Set;
 
 /**
  * This class is the main mod class for VeinMiner. It is loaded as a mod
@@ -52,9 +67,13 @@ public class VeinMiner extends DummyModContainer{
     ConfigurationValues configurationValues;
     public ConfigurationSettings configurationSettings;
 
+    public Logger logger;
+
     @SuppressWarnings("UnusedDeclaration")
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
+        logger = event.getModLog();
+
         configurationValues = new ConfigurationValues(event.getSuggestedConfigurationFile());
         configurationValues.loadConfigFile();
         configurationSettings = new ConfigurationSettings(configurationValues);
@@ -75,5 +94,48 @@ public class VeinMiner extends DummyModContainer{
                 MinerLogger.debug("Enabling debug mode");
             }
         }
+    }
+
+    @SuppressWarnings("UnusedDeclaration")
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+        String[] oreDictList = OreDictionary.getOreNames();
+        for(ToolType toolType : ToolType.values()) {
+            Set<String> autodetectValues = configurationSettings.getAutodetectBlocksList(toolType);
+            if(configurationSettings.getAutodetectBlocksToggle(toolType)) {
+                for(String oreDictEntry : oreDictList) {
+                    for(String autodetectValue : autodetectValues) {
+                        if(!autodetectValue.isEmpty() && oreDictEntry.startsWith(autodetectValue)) {
+                            ArrayList<ItemStack> itemStacks = OreDictionary.getOres(oreDictEntry);
+                            for(ItemStack item : itemStacks) {
+                                if(item.getItem() instanceof ItemBlock) {
+                                    String blockName = Block.blockRegistry.getNameForObject(item.getItem());
+                                    configurationSettings.addBlockToWhitelist(toolType, new BlockID(blockName, item.getItemDamage()));
+                                    try {
+                                        // Some mods raise an exception when calling getDisplayName on blocks.
+                                        MinerLogger.debug("Adding %d:%d (%s) to block whitelist for %s (%s:%s)", blockName, item.getItemDamage(), item.getDisplayName(), toolType.toString(), autodetectValue, oreDictEntry);
+                                    }
+                                    catch (Exception e) {
+                                        logger.error("ERROR while looking at block with name %d. This is a bug with the respective mod.", blockName);
+                                        logger.catching(e);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        configurationSettings.saveConfigs();
+    }
+
+    @SuppressWarnings("UnusedDeclaration")
+    @EventHandler
+    public void serverStarted(FMLServerStartedEvent event) {
+        new MinerServer(configurationValues);
+
+        ServerCommandManager serverCommandManger = (ServerCommandManager) MinecraftServer.getServer().getCommandManager();
+        // TODO: Enable when MinerCommand is fixed.
+        //serverCommandManger.registerCommand(new MinerCommand());
     }
 }
