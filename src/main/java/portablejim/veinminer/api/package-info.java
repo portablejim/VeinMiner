0e3574b5c59ca971f2d05c22e1171e86c90ba3ec
James McKay <dcseee-github@yahoo.com.au>
2015-02-22 08:20:41 +1100

    Add API annotation. That was easy.

---
+++ src/main/java/portablejim/veinminer/api/package-info.java
@@ -0,0 +1,5 @@
+@API(owner = ModInfo.MODID, apiVersion = "0.1", provides = ModInfo.MODID + "Api")
+package portablejim.veinminer.api;
+
+import cpw.mods.fml.common.API;
+import portablejim.veinminer.lib.ModInfo;
