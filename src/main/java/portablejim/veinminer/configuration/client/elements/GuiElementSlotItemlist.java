844ca179a62c82d2faa58178b412cad3a5aff824
James McKay <dcseee-github@yahoo.com.au>
2016-01-11 22:34:15 +1100

    Update function call away from deprecated function.

--- src/main/java/portablejim/veinminer/configuration/client/elements/GuiElementSlotItemlist.java
+++ src/main/java/portablejim/veinminer/configuration/client/elements/GuiElementSlotItemlist.java
@@ -45,7 +45,7 @@ public class GuiElementSlotItemlist extends GuiScrollingList {
     String toolType;
 
     public GuiElementSlotItemlist(ItemlistConfigGuiScreen parent, String toolType) {
-        super(Minecraft.getMinecraft(), parent.width - 10, parent.height, 60, parent.height - 40, 5, 22);
+        super(Minecraft.getMinecraft(), parent.width - 10, parent.height, 60, parent.height - 40, 5, 22, parent.width, parent.height);
         iconRenderer = new IconRenderer(parent.mc, parent.getZLevel());
         this.parent = parent;
         this.toolType = toolType;
