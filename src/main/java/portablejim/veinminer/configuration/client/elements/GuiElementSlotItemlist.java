db31c4c1d335d2cc215634aca12d9ecde96a8710
James McKay <dcseee-github@yahoo.com.au>
2016-01-11 22:33:44 +1100

    Line was useless and caused crashes.

--- src/main/java/portablejim/veinminer/configuration/client/elements/GuiElementSlotItemlist.java
+++ src/main/java/portablejim/veinminer/configuration/client/elements/GuiElementSlotItemlist.java
@@ -17,6 +17,7 @@
 
 package portablejim.veinminer.configuration.client.elements;
 
+import net.minecraft.block.state.IBlockState;
 import net.minecraftforge.fml.client.GuiScrollingList;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
@@ -107,7 +108,6 @@ public class GuiElementSlotItemlist extends GuiScrollingList {
             itemStack.setItemDamage(itemParts.metadata == -1 ? 0 : itemParts.metadata);
 
             // Get new name based on damage value.
-            tryBlock.getStateForEntityRender(tryBlock.getDefaultState());
             displayName = itemStack.getUnlocalizedName();
             iconRenderer.renderItemStackIcon(this.listWidth / 2 - 148, var3 - 1 , itemStack);
         }
