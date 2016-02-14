2d61c167c870296937b8eff8f1bba0d4d239f285
James McKay <dcseee-github@yahoo.com.au>
2016-02-15 00:30:00 +1100

    Add equals() and hashcode() to MinerInstance.
    Should fix memory leak issue ( #45 )

--- src/main/java/portablejim/veinminer/core/MinerInstance.java
+++ src/main/java/portablejim/veinminer/core/MinerInstance.java
@@ -33,6 +33,7 @@ import net.minecraft.util.FoodStats;
 import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 import portablejim.veinminer.VeinMiner;
 import portablejim.veinminer.api.Permission;
 import portablejim.veinminer.api.VeinminerHarvestFailedCheck;
@@ -434,5 +435,51 @@ public class MinerInstance {
         if(startBlacklist.contains(point)) {
             startBlacklist.remove(point);
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        MinerInstance that = (MinerInstance) o;
+
+        if (finished != that.finished) return false;
+        if (numBlocksMined != that.numBlocksMined) return false;
+        if (radiusLimit != that.radiusLimit) return false;
+        if (blockLimit != that.blockLimit) return false;
+        if (serverInstance != null ? !serverInstance.equals(that.serverInstance) : that.serverInstance != null)
+            return false;
+        if (startBlacklist != null ? !startBlacklist.equals(that.startBlacklist) : that.startBlacklist != null)
+            return false;
+        if (destroyQueue != null ? !destroyQueue.equals(that.destroyQueue) : that.destroyQueue != null) return false;
+        if (awaitingEntityDrop != null ? !awaitingEntityDrop.equals(that.awaitingEntityDrop) : that.awaitingEntityDrop != null)
+            return false;
+        if (drops != null ? !drops.equals(that.drops) : that.drops != null) return false;
+        if (world != null ? !world.equals(that.world) : that.world != null) return false;
+        if (player != null ? !player.equals(that.player) : that.player != null) return false;
+        if (targetBlock != null ? !targetBlock.equals(that.targetBlock) : that.targetBlock != null) return false;
+        if (usedItem != null ? !usedItem.equals(that.usedItem) : that.usedItem != null) return false;
+        return initalBlock != null ? initalBlock.equals(that.initalBlock) : that.initalBlock == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = serverInstance != null ? serverInstance.hashCode() : 0;
+        result = 31 * result + (startBlacklist != null ? startBlacklist.hashCode() : 0);
+        result = 31 * result + (destroyQueue != null ? destroyQueue.hashCode() : 0);
+        result = 31 * result + (awaitingEntityDrop != null ? awaitingEntityDrop.hashCode() : 0);
+        result = 31 * result + (drops != null ? drops.hashCode() : 0);
+        result = 31 * result + (world != null ? world.hashCode() : 0);
+        result = 31 * result + (player != null ? player.hashCode() : 0);
+        result = 31 * result + (targetBlock != null ? targetBlock.hashCode() : 0);
+        result = 31 * result + (finished ? 1 : 0);
+        result = 31 * result + (usedItem != null ? usedItem.hashCode() : 0);
+        result = 31 * result + numBlocksMined;
+        result = 31 * result + (initalBlock != null ? initalBlock.hashCode() : 0);
+        result = 31 * result + radiusLimit;
+        result = 31 * result + blockLimit;
+        return result;
     }
 }
