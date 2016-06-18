c959154b394aa91151980bdc95d75f2cdbf5440d
James McKay <dcseee-github@yahoo.com.au>
2016-06-18 10:49:32 +1000

    Bump API version to 0.3.

--- src/main/java/portablejim/veinminer/api/package-info.java
+++ src/main/java/portablejim/veinminer/api/package-info.java
@@ -1,4 +1,4 @@
-@API(owner = ModInfo.MODID, apiVersion = "0.1", provides = ModInfo.MODID + "Api")
+@API(owner = ModInfo.MODID, apiVersion = "0.3", provides = ModInfo.MODID + "Api")
 package portablejim.veinminer.api;
 
 import cpw.mods.fml.common.API;
