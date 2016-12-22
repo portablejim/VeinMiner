8257dccdd59fed8d497eee07d3d576ebbe130da4
James McKay <dcseee-github@yahoo.com.au>
2016-12-22 16:11:12 +1100

    Make EntityItem checking stricter.
    Closes #91

--- src/main/java/portablejim/veinminer/core/EntityDropHook.java
+++ src/main/java/portablejim/veinminer/core/EntityDropHook.java
@@ -70,7 +70,7 @@ public class EntityDropHook {
             return;
         }
 
-        if(!EntityItem.class.isInstance(entity)) {
+        if(!(EntityItem.class == entity.getClass())) {
             return;
         }
 
